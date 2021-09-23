724884b3e953e1b70d4ed9792dfe54d4ee3b5dac
Adimarantis <joerg.bublath@intel.com>
2021-09-23 20:36:08 +0200

    new package structure

--- build-signal-cli-packages.sh
+++ build-signal-cli-packages.sh
@@ -1,35 +1,33 @@
 #!/bin/bash
-echo "This script expects that signal-cli is freshly build and has the x86 libraries included"
+echo "This script expects that signal-cli is freshly build"
+LIBSOURCE=~/src/FHEM-Signalbot
 SIGNALSOURCE=~/src/signal-cli-main
 VERSION=0.9.0
-DIR=`pwd`
-mkdir ~/signal-cli-dbus_$VERSION-1_amd64
-mkdir ~/signal-cli-dbus_$VERSION-1_armhf
-cp -r package/* ~/signal-cli-dbus_$VERSION-1_amd64
-cp -r package/* ~/signal-cli-dbus_$VERSION-1_armhf
+echo "Version $VERSION , signal-cli in $SIGNALSOURCE , binary libs in $LIBSOURCE"
 
-#change Architectur in amd64 version
-sed -i 's/armhf/amd64/' ~/signal-cli-dbus_$VERSION-1_amd64/DEBIAN/control
+make_package() {
+	cd $LIBSOURCE
+	ARCH=$1
+	GLIB=$2
+	DIR=signal-cli-dbus_$VERSION-1_$GLIB\_$ARCH
+	echo "Creating archive for $DIR"
+	rm -r ~/$DIR
+	mkdir ~/$DIR
+	cp -r package/* ~/$DIR
+	if [ $ARCH = "amd64" ]; then
+		sed -i 's/armhf/amd64/' ~/$DIR/DEBIAN/control
+	fi
+	cp -r $SIGNALSOURCE/build/install/signal-cli/* ~/$DIR/opt/signal
+	cp $ARCH-$GLIB-$VERSION/*.so ~/$DIR/opt/signal/lib
+	cd ~/$DIR/opt/signal/lib
+	zip -u signal-client-java-*.jar libsignal_jni.so
+	zip -u zkgroup-java-*.jar libzkgroup.so
+	rm *.so
+	cd ~/
+	dpkg-deb --build --root-owner-group $DIR
+}
 
-#Copy signal-cli files
-cp -r $SIGNALSOURCE/build/install/signal-cli/* ~/signal-cli-dbus_$VERSION-1_amd64/opt/signal
-cp -r $SIGNALSOURCE//build/install/signal-cli/* ~/signal-cli-dbus_$VERSION-1_armhf/opt/signal
-
-#Copy ARM specific libraries
-cp armv7l-$VERSION/*.so ~/signal-cli-dbus_$VERSION-1_armhf/opt/signal/lib
-cd ~/signal-cli-dbus_$VERSION-1_armhf/opt/signal/lib
-zip -u signal-client-java-*.jar libsignal_jni.so
-zip -u zkgroup-java-*.jar libzkgroup.so
-rm *.so
-cd ~/
-dpkg-deb --build --root-owner-group signal-cli-dbus_$VERSION-1_armhf
-dpkg-deb --build --root-owner-group signal-cli-dbus_$VERSION-1_amd64
-mv signal-cli-dbus_$VERSION-1_amd64.deb signal-cli-dbus_$VERSION-1_amd64-u20.deb
-cd $DIR
-cp amd64-$VERSION/*.so ~/signal-cli-dbus_$VERSION-1_amd64/opt/signal/lib
-cd ~/signal-cli-dbus_$VERSION-1_amd64/opt/signal/lib
-zip -u signal-client-java-*.jar libsignal_jni.so
-zip -u zkgroup-java-*.jar libzkgroup.so
-rm *.so
-cd ~/
-dpkg-deb --build --root-owner-group signal-cli-dbus_$VERSION-1_amd64
+make_package amd64 glibc2.28
+make_package amd64 glibc2.27
+make_package amd64 glibc2.31
+make_package armhf glibc2.28

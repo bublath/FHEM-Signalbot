59d9b9cd1cb53577c25c7230a7363e2981437b26
Adimarantis <joerg.bublath@intel.com>
2022-01-16 13:10:10 +0100

    version cleanup, removed isRegistered, more flexible get+set lists

--- 50_Signalbot.pm
+++ 50_Signalbot.pm
@@ -27,34 +27,6 @@ use HttpUtils;
 eval "use Protocol::DBus;1";
 eval "use Protocol::DBus::Client;1" or my $DBus_missing = "yes";
 
-my %sets = (
-  "send" => "textField",
-  "reinit" => "noArg",
-  "signalAccount" => "none",		#V0.9.0+
-  "contact" => "textField",
-  "createGroup" => "textField",		#Call updategroups with empty group parameter, mandatory name and optional avatar picture
-  "invite" => "textField",			#Call updategroups with mandatory group name and mandatory list of numbers to join
-  "block" => "textField",			#Call setContactBlocked or setGroupBlocked (one one at a time)
-  "unblock" => "textField",			#Call setContactBlocked or setGroupBlocked (one one at a time)
-  "updateGroup" => "textField",		#Call updategroups to rename a group and/or set avatar picture
-  "quitGroup" => "textField",		#V0.8.1+
-  "joinGroup" => "textField",		#V0.8.1+
-  "updateProfile" => "textField",	#V0.8.1+
-  "link" => "noArg",				#V0.9.0+
-  "register" => "textField",		#V0.9.0+
-  "captcha" => "textField",			#V0.9.0+
-  "verify" => "textField",			#V0.9.0+
- );
- 
- my %gets = (
-  "contacts"      => "all,nonblocked",			#V0.8.1+
-  "groups"        => "all,active,nonblocked",	#V0.8.1+
-  "accounts"      => "noArg",					#V0.9.0+
-  "favorites"     => "noArg",
-#  "introspective" => "noArg"
-);
-
-
 #maybe really get introspective here instead of handwritten list
  my %signatures = (
 	"setContactBlocked" 	=> "sb",
@@ -78,7 +50,6 @@ my %sets = (
 	"version" 				=> "",
 	"isMember"				=> "ay",
 	"getSelfNumber"			=> "", #V0.9.1
-#	"isRegistered" 			=> "", Removing this will primarily use the register instance, so "true" means -U mode, "false" means multi
 	"sendEndSessionMessage" => "as",		#unused
 	"sendRemoteDeleteMessage" => "xas",		#unused
 	"sendGroupRemoteDeletemessage" => "xay",#unused
@@ -89,7 +60,6 @@ my %sets = (
 #dbus interfaces that only exist in registration mode
 my %regsig = (
 	"listAccounts"			=> "",
-	"isRegistered"			=> "",
 	"link"					=> "s",
 	"registerWithCaptcha"	=> "sbs",	#not implemented yet
 	"verifyWithPin"			=> "sss",	#not implemented yet
@@ -145,19 +115,32 @@ sub Signalbot_Set($@) {					#
 	my $cmd = shift @args;
 	my $account = ReadingsVal($name,"account","none");
 	my $version = $hash->{helper}{version};
-	if (!exists($sets{$cmd}))  {
-		my @cList;
-		foreach my $k (keys %sets) {
-			my $opts = undef;
-			$opts = $sets{$k};
-
-			if (defined($opts)) {
-				push(@cList,$k . ':' . $opts);
-			} else {
-				push (@cList,$k);
-			}
-		}
-		return "Signalbot_Set: Unknown argument $cmd, choose one of " . join(" ", @cList);
+	my $multi = $hash->{helper}{multi};
+	my @accounts;
+	@accounts =@{$hash->{helper}{accountlist}} if defined $hash->{helper}{accountlist};
+	my $sets=	"send:textField ".
+				"reinit:noArg ".
+				"contact:textField ".
+				"createGroup:textField ".
+				"invite:textField ".
+				"block:textField ".
+				"unblock:textField ".
+				"updateGroup:textField ".
+				"quitGroup:textField ".
+				"joinGroup:textField ".
+				"updateProfile:textField ";
+	my $sets_reg=	"link:noArg ".
+					"register:textField ".
+					"captcha:textField ".
+					"verify:textField ";
+					
+	
+	$sets.=$sets_reg if $multi==1;
+	$sets=$sets_reg if $account eq "none";
+	$sets.="signalAccount:".join(",",@accounts) if @accounts>0; 
+	
+	if ( $cmd eq "?" ) {
+		return "Signalbot_Set: Unknown argument $cmd, choose one of " . $sets;
 	} # error unknown cmd handling
 	
 	# Works always
@@ -275,9 +258,6 @@ sub Signalbot_Set($@) {					#
 		}
 		return undef;
 	} elsif ( $cmd eq "updateProfile") {
-		if ($version<=800) {
-			return "updateProfile requires signal-cli 0.8.1 or higher";
-		}
 		if (@args<1 || @args>2 ) {
 			return "Usage: set ".$hash->{NAME}." updateProfile <new name> &[path to thumbnail]";
 		} else {
@@ -286,9 +266,6 @@ sub Signalbot_Set($@) {					#
 		}
 		return undef;
 	} elsif ( $cmd eq "quitGroup") {
-		if ($version<=800) {
-			return $cmd." requires signal-cli 0.8.1 or higher";
-		}
 		if (@args!=1) {
 			return "Usage: set ".$hash->{NAME}." ".$cmd." <group name>";
 		}
@@ -443,21 +420,14 @@ sub Signalbot_Get($@) {
 	return "Signalbot_Get: No cmd specified for get" if ( $numberOfArgs < 1 );
 
 	my $cmd = shift @args;
+	my $account = ReadingsVal($name,"account","none");
 
-	if (!exists($gets{$cmd}))  {
-		my @cList;
-		foreach my $k (keys %gets) {
-			my $opts = undef;
-			$opts = $gets{$k};
-
-			if (defined($opts)) {
-				push(@cList,$k . ':' . $opts);
-			} else {
-				push (@cList,$k);
-			}
-		}
-		return "Signalbot_Get: Unknown argument $cmd, choose one of " . join(" ", @cList);
-	} # error unknown cmd handling
+	if ($cmd eq "?") {
+		my $gets="favorites:noArg accounts:noArg ";
+		$gets.="contacts:all,nonblocked ".
+			"groups:all,active,nonblocked " if $account ne "none";
+		return "Signalbot_Get: Unknown argument $cmd, choose one of ".$gets;
+	}
 	
 	my $version = $hash->{helper}{version};
 	my $arg = shift @args;
@@ -466,17 +436,11 @@ sub Signalbot_Get($@) {
 		my $reply=Signalbot_CallS($hash,"org.freedesktop.DBus.Introspectable.Introspect");
 		return undef;
 	} elsif ($cmd eq "accounts") {
-		if ($version<900) {
-			return "Signal-cli 0.9.0 required for this functionality";
-		}
-		my $num=Signalbot_CallS($hash,"listAccounts");
-		return "Error in listAccounts" if !defined $num;
-		my @numlist=@$num;
+		my $num=Signalbot_getAccounts($hash);
+		return "Error in listAccounts" if $num<0;
 		my $ret="List of registered accounts:\n\n";
-		foreach my $number (@numlist) {
-			my $account = $number =~ s/\/org\/asamk\/Signal\/_/+/rg;
-			$ret.=$account."\n";
-		}
+		my @numlist=@{$hash->{helper}{accountlist}};
+		$ret=$ret.join("\n",@numlist);
 		return $ret;
 	} elsif ($cmd eq "favorites") {
 		my $favs = AttrVal($name,"favorites","");
@@ -504,66 +468,54 @@ sub Signalbot_Get($@) {
 		}
 		$ret.="\n(A)=GoogleAuth required to execute command";
 		return $ret;
-	}
-	
-	if ($gets{$cmd} =~ /$arg/) {
-		if ($cmd eq "contacts") {
-			if ($version<=800) {
-				return "Signal-cli 0.8.1+ required for this functionality";
+	} elsif ($cmd eq "contacts" && defined $arg) {
+		my $num=Signalbot_CallS($hash,"listNumbers");
+		return "Error in listNumbers" if !defined $num;
+		my @numlist=@$num;
+		my $ret="List of known contacts:\n\n";
+		my $format="%-16s|%-30s|%-6s\n";
+		$ret.=sprintf($format,"Number","Name","Blocked");
+		$ret.="\n";
+		foreach my $number (@numlist) {
+			my $blocked=Signalbot_CallS($hash,"isContactBlocked",$number);
+			my $name=$hash->{helper}{contacts}{$number};
+			$name="UNKNOWN" unless defined $name;
+			if (! ($number =~ /^\+/) ) { $number="Unknown"; }
+			if ($arg eq "all" || $blocked==0) {
+				$ret.=sprintf($format,$number,$name,$blocked==1?"yes":"no");
 			}
-			my $num=Signalbot_CallS($hash,"listNumbers");
-			return "Error in listNumbers" if !defined $num;
-			my @numlist=@$num;
-			my $ret="List of known contacts:\n\n";
-			my $format="%-16s|%-30s|%-6s\n";
-			$ret.=sprintf($format,"Number","Name","Blocked");
-			$ret.="\n";
-			foreach my $number (@numlist) {
-				my $blocked=Signalbot_CallS($hash,"isContactBlocked",$number);
-				my $name=$hash->{helper}{contacts}{$number};
-				$name="UNKNOWN" unless defined $name;
-				if (! ($number =~ /^\+/) ) { $number="Unknown"; }
-				if ($arg eq "all" || $blocked==0) {
-					$ret.=sprintf($format,$number,$name,$blocked==1?"yes":"no");
-				}
-			}
-			return $ret;
-		} elsif ($cmd eq "groups") {
-			Signalbot_refreshGroups($hash);
-			if ($version<=800) {
-				return "Signal-cli 0.8.1+ required for this functionality";
-			}
-			
-			my $ret="List of known groups:\n\n";
-			my $memsize=40;
-			my $format="%-20.20s|%-6.6s|%-7.7s|%-".$memsize.".".$memsize."s\n";
-			$ret.=sprintf($format,"Group","Active","Blocked","Members");
-			$ret.="\n";
-			foreach my $groupid (keys %{$hash->{helper}{groups}}) {
-				my $blocked=$hash->{helper}{groups}{$groupid}{blocked};
-				my $active=$hash->{helper}{groups}{$groupid}{active};
-				my $group=$hash->{helper}{groups}{$groupid}{name};
-				my @groupID=split(" ",$groupid);
-				my $mem=Signalbot_CallS($hash,"getGroupMembers",\@groupID);
-				return "Error reading group memebers" if !defined $mem;
-				my @members=();;
-				foreach (@$mem) {
-					push @members,Signalbot_getContactName($hash,$_);
-				}
-				if ($arg eq "all" || ($active==1 && $arg eq "active") || ($active==1 && $blocked==0 && $arg eq "nonblocked") ) {
-					my $mem=join(",",@members);
-					$ret.=sprintf($format,$group,$active==1?"yes":"no",$blocked==1?"yes":"no",substr($mem,0,$memsize));
-					my $cnt=1;
-					while (length($mem)>$cnt*$memsize) {
-						$ret.=sprintf($format,"","","",substr($mem,$cnt*$memsize,$cnt*$memsize+$memsize));
-						$cnt++;
-					}
-				}
-			}
-			return $ret;
 		}
-	} else {
-		return "Signalbot_Set: Unknown argument for $cmd : $arg";
+		return $ret;
+	} elsif ($cmd eq "groups" && defined $arg) {
+		Signalbot_refreshGroups($hash);
+		
+		my $ret="List of known groups:\n\n";
+		my $memsize=40;
+		my $format="%-20.20s|%-6.6s|%-7.7s|%-".$memsize.".".$memsize."s\n";
+		$ret.=sprintf($format,"Group","Active","Blocked","Members");
+		$ret.="\n";
+		foreach my $groupid (keys %{$hash->{helper}{groups}}) {
+			my $blocked=$hash->{helper}{groups}{$groupid}{blocked};
+			my $active=$hash->{helper}{groups}{$groupid}{active};
+			my $group=$hash->{helper}{groups}{$groupid}{name};
+			my @groupID=split(" ",$groupid);
+			my $mem=Signalbot_CallS($hash,"getGroupMembers",\@groupID);
+			return "Error reading group memebers" if !defined $mem;
+			my @members=();;
+			foreach (@$mem) {
+				push @members,Signalbot_getContactName($hash,$_);
+			}
+			if ($arg eq "all" || ($active==1 && $arg eq "active") || ($active==1 && $blocked==0 && $arg eq "nonblocked") ) {
+				my $mem=join(",",@members);
+				$ret.=sprintf($format,$group,$active==1?"yes":"no",$blocked==1?"yes":"no",substr($mem,0,$memsize));
+				my $cnt=1;
+				while (length($mem)>$cnt*$memsize) {
+					$ret.=sprintf($format,"","","",substr($mem,$cnt*$memsize,$cnt*$memsize+$memsize));
+					$cnt++;
+				}
+			}
+		}
+		return $ret;
 	}
 	return undef;
 }
@@ -655,7 +607,6 @@ sub Signalbot_command($@){
 				}
 			} else {
 				my $ret="Defined favorites:\n\n";
-				my $format="%2i %-30s\n";
 				$ret.="ID [Alias] Command\n";
 				my $cnt=1;
 				foreach (@favorites) {
@@ -663,9 +614,10 @@ sub Signalbot_command($@){
 					$ff =~ /(^\[(.*?)\])?([\-]?)(.*)/;
 					my $fav=$4;
 					$fav =~ s/##/;;/g;
-					$fav=substr($fav,0,18)."..." if length($fav)>18;
-					$fav=$1." ".$fav if defined $1;
-					$ret.=sprintf($format,$cnt,$fav);
+					$fav =$1." ".$fav if defined $1;
+					$fav =sprintf("%2i %s",$cnt,$fav);
+					$fav =substr($fav,0,25)."..." if length($fav)>27;
+					$ret.=$fav."\n";
 					$cnt++;
 				}
 				$ret="No favorites defined" if @favorites==0;
@@ -732,11 +684,10 @@ sub Signalbot_MessageReceivedV2 ($@) {
 	my ($hash,$timestamp,$source,$groupID,$message,$extras) = @_;
 	Log3 $hash->{NAME}, 5, $hash->{NAME}.": Message CallbackV2 - ignored";	
 	return;
-	my $att = $extras->{attachments};
 	my @attachments;
-	if (defined $att) {
-		my @arr=@$att;
-		foreach (@arr) {
+	if (defined $extras->{attachments}) {
+		my @att =@{$extras->{attachments}};
+		foreach (@att) {
 			push @attachments, $_->{file};
 		}
 	}
@@ -986,81 +937,55 @@ sub Signalbot_setup2($@) {
 	$hash->{helper}{version}=$ver[0]*10000+$ver[1]*100+$ver[2];
 	$hash->{VERSION}="Signalbot:".$Signalbot_VERSION." signal-cli:".$version." Protocol::DBus:".$Protocol::DBus::VERSION;
 	$hash->{model}=Signalbot_OSRel();
-	if($hash->{helper}{version}>800) {
-		my $state=Signalbot_CallS($hash,"isRegistered");
-		#Signal-cli 0.9.0 : isRegistered not existing and will return undef when in multi-mode (or false with my PR)
-		if (!defined $state) {$state=0;}
-		if ($state) {
-			#if running daemon is running in -u mode set the signal path to default regardless of the registered number
-			$hash->{helper}{signalpath}='/org/asamk/Signal';
-			$account=Signalbot_CallS($hash,"getSelfNumber"); #Available from signal-cli 0.9.1
-			#Workaround since signal-cli 0.9.0 did not include by getSelfNumber() method - delete the reading, so its not confusing
-			if (!defined $account) { readingsDelete($hash,"account"); }
-			#Remove all entries that are only available in registration or multi mode
-			delete($gets{accounts});
-			delete($sets{signalAccount});
-			delete($sets{register});
-			delete($sets{captcha});
-			delete($sets{verify});
-			delete($sets{link});
-			$hash->{helper}{accounts}=0;
-			$hash->{helper}{multi}=0;
-		} else {
-			#else get accounts and add all numbers to the pulldown
-			my $num=Signalbot_CallS($hash,"listAccounts");
-			return "Error in listAccounts" if !defined $num;
-			my @numlist=@$num;
-			my $ret="";
-			foreach my $number (@numlist) {
-				if ($ret ne "") {$ret.=",";}
-				$ret .= $number =~ s/\/org\/asamk\/Signal\/_/+/rg;
-			}
-			$sets{signalAccount}=$ret eq ""?"none":$ret;
-			#Only one number existing - choose automatically if not already set)
-			if(@numlist == 1 && $account eq "none") {
-				Signalbot_setAccount($hash,$ret);
-				$account=$ret;
-			}
-			$hash->{helper}{accounts}=(@numlist);
-			$hash->{helper}{multi}=1;
-		}
-		#Initialize Signal listener only at the very end to make sure correct $signalpath is used
-		my $signalpath=$hash->{helper}{signalpath};
-		$dbus->send_call(
-				path        => '/org/freedesktop/DBus',
-				interface   => 'org.freedesktop.DBus',
-				member      => 'AddMatch',
-				destination => 'org.freedesktop.DBus',
-				signature   => 's',
-				body        => [ "type='signal',path='$signalpath'" 
-			],
-			);	
-		$hash->{STATE}="Connected to $signalpath";
-
-		#-u Mode or already registered
-		if ($state || $account ne "none") {
-			Signalbot_Call($hash,"listNumbers");
-			#Might make sense to call refreshGroups here, however, that is a sync call and might potentially take longer, so maybe no good idea in startup
-			readingsBeginUpdate($hash);
-			readingsBulkUpdate($hash, 'account', $account) if defined $account;
-			readingsBulkUpdate($hash, 'lastError', "ok");
-			readingsEndUpdate($hash, 1);
-			return undef;
-		} else {
-			readingsBeginUpdate($hash);
-			readingsBulkUpdate($hash, 'account', "none");
-			readingsBulkUpdate($hash, 'joinedGroups', "");
-			readingsBulkUpdate($hash, 'lastError', "No account registered - use set account to connect to an existing registration, link or register to get a new account");
-			readingsEndUpdate($hash, 1);
-			return undef;
-		}
+	#listAccounts only available in registration instance
+	my $num=Signalbot_getAccounts($hash);
+	#listAccounts failed - probably running in -u mode
+	if ($num<0) {
+		$hash->{helper}{signalpath}='/org/asamk/Signal';
+		$account=Signalbot_CallS($hash,"getSelfNumber") if ($version>901); #Available from signal-cli 0.9.1
+		#Workaround since signal-cli 0.9.0 did not include by getSelfNumber() method - delete the reading, so its not confusing
+		if (!defined $account) { readingsDelete($hash,"account"); }
+		$hash->{helper}{accounts}=0;
+		$hash->{helper}{multi}=0;
 	} else {
-		#These require 0.8.1 or greater
-		delete ($gets{contacts});
-		delete ($gets{groups});
-		delete ($sets{updateProfile});
-		delete ($sets{quitGroup});
-		delete ($sets{joinGroup});
+		my @numlist=@{$hash->{helper}{accountlist}};
+		#Only one number existing - choose automatically if not already set)
+		if(@numlist == 1 && $account eq "none") {
+			Signalbot_setAccount($hash,$numlist[0]);
+			$account=$numlist[0];
+		}
+		$hash->{helper}{multi}=1;
+	}
+	
+	#Initialize Signal listener only at the very end to make sure correct $signalpath is used
+	my $signalpath=$hash->{helper}{signalpath};
+	$dbus->send_call(
+			path        => '/org/freedesktop/DBus',
+			interface   => 'org.freedesktop.DBus',
+			member      => 'AddMatch',
+			destination => 'org.freedesktop.DBus',
+			signature   => 's',
+			body        => [ "type='signal',path='$signalpath'" 
+		],
+		);	
+	$hash->{STATE}="Connected to $signalpath";
+
+	#-u Mode or already registered
+	if ($num<0 || $account ne "none") {
+		Signalbot_Call($hash,"listNumbers");
+		#Might make sense to call refreshGroups here, however, that is a sync call and might potentially take longer, so maybe no good idea in startup
+		readingsBeginUpdate($hash);
+		readingsBulkUpdate($hash, 'account', $account) if defined $account;
+		readingsBulkUpdate($hash, 'lastError', "ok");
+		readingsEndUpdate($hash, 1);
+		return undef;
+	} else {
+		readingsBeginUpdate($hash);
+		readingsBulkUpdate($hash, 'account', "none");
+		readingsBulkUpdate($hash, 'joinedGroups', "");
+		readingsBulkUpdate($hash, 'lastError', "No account registered - use set account to connect to an existing registration, link or register to get a new account");
+		readingsEndUpdate($hash, 1);
+		return undef;
 	}
 }
 
@@ -1113,8 +1038,8 @@ sub Signalbot_CallS($@) {
 		$got_response = 1;
 		} 
 	) -> catch ( sub () {
-		#Ignore this error in Log for isRegistered since it is expected and confuses users
-		if ($function ne "isRegistered") {
+		#Ignore this error in Log for listAccounts since it is expected and confuses users
+		if ($function ne "listAccounts") {
 			Log3 $hash->{NAME}, 4, $hash->{NAME}.": Sync Error for: $function";
 		}
 		my $msg = shift;
@@ -1623,6 +1548,21 @@ sub Signalbot_checkNumber($) {
 	return undef;
 }
 
+sub Signalbot_getAccounts($) {
+	my ($hash) = @_;
+	my $num=Signalbot_CallS($hash,"listAccounts");
+	return -1 if !defined $num;
+	my @numlist=@$num;
+	my @accounts;
+	foreach my $number (@numlist) {
+		my $account = $number =~ s/\/org\/asamk\/Signal\/_/+/rg;
+		push @accounts,$account;
+	}
+	$hash->{helper}{accountlist}=\@accounts;
+	$hash->{helper}{accounts}=@accounts;
+	return @accounts;
+}
+
 sub Signalbot_setAccount($$) {
 	my ($hash,$account) = @_;
 	$account = Signalbot_checkNumber($account);
@@ -1805,7 +1745,7 @@ sub Signalbot_Detail {
 		$ret .= "      and X86 or armv7l CPUs<br>";
 	}
 	if($version==901) {
-		$ret .= "<b>Warning: signal-cli v0.9.1 has issues affecting Signalbot.</b><br>Please use installer to downgrade to 0.9.0<br>";
+		$ret .= "<b>Warning: signal-cli v0.9.1 has issues affecting Signalbot.</b><br>Please use installer to upgrade to 0.9.2+<br>";
 	}
 	if ($multi==0 && $version>0) {
 		$ret .= "Signal-cli is running in single-mode, please consider starting it without -u parameter (e.g. by re-running the installer)<br>";
